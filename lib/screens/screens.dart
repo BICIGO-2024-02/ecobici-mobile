d812835f24809cb06dd721085a5fa726413f4fd6
Gonzalo Sakuda <gonchissn@gmail.com>
2024-10-15 22:59:46 -0500

    Create signin screen

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -1,2 +1,4 @@
 export 'package:ecobicimobileapp/screens/home_screen.dart';
 export 'package:ecobicimobileapp/screens/bicycle_search_screen.dart';
+export 'package:ecobicimobileapp/screens/register_screen.dart';
+export 'package:ecobicimobileapp/screens/signin_screen.dart';
